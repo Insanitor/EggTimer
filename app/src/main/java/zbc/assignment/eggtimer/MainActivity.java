76943a7c7487ccca72ec1509227b76f5639e11d9
Dennis Dupont <TheInsanitor@hotmail.com>
2019-03-04 18:12:34 +0100

    Did alot

--- app/src/main/java/zbc/assignment/eggtimer/MainActivity.java
+++ app/src/main/java/zbc/assignment/eggtimer/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,27 +13,31 @@ public class MainActivity extends AppCompatActivity {
     EggTimer eggTimer;
 
     private TextView tv;
-    private Button startButton, softButton, smileButton, hardButton;
+    private Button startButton;
+    private ImageButton softButton, smileButton, hardButton;
 
     enum EggType {Soft, Smiling, Hard}
 
     EggType currentEgg;
     final Handler handler = new Handler();
-
+    static boolean firstCreate = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firstCreate = true;
         tv = findViewById(R.id.TimerText);
         startButton = findViewById(R.id.StartButton);
         softButton = findViewById(R.id.SoftBoilButton);
         smileButton = findViewById(R.id.SmilingButton);
         hardButton = findViewById(R.id.HardButton);
         eggTimer = new EggTimer(tv);
+
     }
 
     public void OnEggSelectClicked(View view) {
+
         switch (view.getId()) {
             case R.id.SoftBoilButton: {
 
@@ -69,33 +74,33 @@ public class MainActivity extends AppCompatActivity {
 
     public void onStartClick(final View view) {
         if (eggTimer.getTimer() > 0) {
-            handler.postDelayed(eggTimer, 1000);
-            startButton.setText("Stop");
-            softButton.setEnabled(false);
-            smileButton.setEnabled(false);
-            hardButton.setEnabled(false);
             startButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     onStop(view);
                 }
             });
+            softButton.setEnabled(false);
+            smileButton.setEnabled(false);
+            hardButton.setEnabled(false);
+            startButton.setText("Stop");
+            handler.postDelayed(eggTimer, 1000);
         }
     }
 
     public void onStop(final View view) {
         eggTimer.setTimer(0);
-        startButton.setText("Start");
-        softButton.setEnabled(true);
-        smileButton.setEnabled(true);
-        hardButton.setEnabled(true);
-        tv.setText("0:00");
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onStartClick(view);
             }
         });
+        startButton.setText("Start");
+        softButton.setEnabled(true);
+        smileButton.setEnabled(true);
+        hardButton.setEnabled(true);
+        tv.setText("0:00");
 
     }
 
